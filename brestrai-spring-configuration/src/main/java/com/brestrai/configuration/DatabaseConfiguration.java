7383ba125126fabe3687f0637c44e3bd25eea084
Bruno <brunoestrai@gmail.com>
2024-03-05 22:53:01 -0300

    Colocado rodar o template

--- brestrai-spring-configuration/src/main/java/com/brestrai/configuration/DatabaseConfiguration.java
+++ brestrai-spring-configuration/src/main/java/com/brestrai/configuration/DatabaseConfiguration.java
@@ -1,8 +1,33 @@
 package com.brestrai.configuration;
 
+import com.brestrai.utils.template.configurations.database.JpaConfiguration;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseConfiguration extends JpaConfiguration {
 
+    @Override
+    public String setEntityScanPackage() {
+        return null;
+    }
+
+    @Override
+    public String setDriver() {
+        return null;
+    }
+
+    @Override
+    public String setUrl() {
+        return null;
+    }
+
+    @Override
+    public String setUsername() {
+        return null;
+    }
+
+    @Override
+    public String setPassword() {
+        return null;
+    }
 }
