878a61a8a933157c7877e0b41dace1ab96148f69
Bruno <brunoestrai@gmail.com>
2024-03-07 01:16:18 -0300

    Entregue base

--- brestrai-spring-configuration/src/main/java/com/brestrai/configuration/RabbitMqComponent.java
+++ brestrai-spring-configuration/src/main/java/com/brestrai/configuration/RabbitMqComponent.java
@@ -1,6 +1,17 @@
 package com.brestrai.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class RabbitMqComponent {
+
+    @Value("${app.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${app.rabbitmq.routing-key}")
+    private String routingKey;
 
     // TODO: Implement RabbitMqComponent
 
